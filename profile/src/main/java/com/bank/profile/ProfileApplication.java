90ffbe031808fbf1f0b45ee170d67684ee3f0e7c
Revelax <148916670+Revelax@users.noreply.github.com>
2024-02-24 13:10:46 +0300

    Подключил GlobalRestHandler: pom.xml,ProfileApplication.

--- profile/src/main/java/com/bank/profile/ProfileApplication.java
+++ profile/src/main/java/com/bank/profile/ProfileApplication.java
@@ -3,7 +3,9 @@ package com.bank.profile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.bank.common")
 @EnableEurekaClient
 @SpringBootApplication
 public class ProfileApplication {
