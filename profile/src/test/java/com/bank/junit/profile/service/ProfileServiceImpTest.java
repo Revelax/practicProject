99b7adbc3a12fc6ba0436115aaef8d83c5fd7d6b
Revelax <148916670+Revelax@users.noreply.github.com>
2024-02-26 13:06:37 +0300

    Начал изучать тесты.

---
+++ profile/src/test/java/com/bank/junit/profile/service/ProfileServiceImpTest.java
@@ -0,0 +1,12 @@
+package com.bank.junit.profile.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ProfileServiceImpTest {
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
