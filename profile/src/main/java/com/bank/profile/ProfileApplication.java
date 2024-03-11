2a985015fde2aad4f3e5fbbb042d7f3f2270aade
Revelax <148916670+Revelax@users.noreply.github.com>
2024-03-11 20:53:54 +0300

    Небольшая правка неудачного слияния.

--- profile/src/main/java/com/bank/profile/ProfileApplication.java
+++ profile/src/main/java/com/bank/profile/ProfileApplication.java
@@ -3,7 +3,6 @@ package com.bank.profile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
 
 @EnableEurekaClient
 @SpringBootApplication(scanBasePackages = {"com.bank.common", "com.bank.profile"})
